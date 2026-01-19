673a0f991c6f35efb08c69db1a4be1409033340b
Jeunard Boekhoudt <j.boekhoudt@bereslim.nl>
2026-01-19 20:15:03 +0100

    chore: modifications to workflow

--- src/test/java/com/example/opentrivia/controller/TriviaControllerIT.java
+++ src/test/java/com/example/opentrivia/controller/TriviaControllerIT.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class TriviaControllerIT{
 
